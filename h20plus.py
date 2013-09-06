b7a6d5ab2771109e01249b6ccdda4f5bd8e054af
Spencer Lee <lesp@seas.upenn.edu>
2013-09-06 19:17:31 -0400

    Updated h20plus.py

--- h20plus.py
+++ h20plus.py
@@ -10,4 +10,4 @@ def mainPage():
 #Only run the app if the script is run from the main python interpreter
 if __name__ == '__main__': 
   port=int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0',port=port)
+  app.run()
